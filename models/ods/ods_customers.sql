0ed694f6ee3b65127ccd8d4811d086d078603c1e
AMIT VERMA <av.amitverma2910@gmail.com>
2026-07-18 12:50:28 +0000

    update and fix lnd-->ods

--- models/ods/ods_customers.sql
+++ models/ods/ods_customers.sql
@@ -1,5 +1,15 @@
 
 
+SELECT
+    CUSTOMER_ID,
+    CUSTOMER_NAME,
+    EMAIL,
+    CITY,
+    STATE,
+    SIGNUP_DATE,
+    STATUS,
+    LAST_UPDATED
+FROM {{ ref('lnd_customers') }}
 
 
-select * from {{ ref('customers')}}
+
